9d1b3dc548fd4beef0a8a0b289ad006601aba091
Nishan Raut <nishan.raut1709@gmail.com>
2025-06-23 21:04:36 +0545

    Creating index

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type indexValue struct {
+	offset int
+	length int
+}
+
 func main() {
-	fmt.Println("This is disk query")
+	fmt.Println("This is disk read")
+	var index map[int]indexValue
+	fmt.Println("This is index: ", index)
+	PopulateIndex(&index)
+}
+
+func PopulateIndex(index *map[int]indexValue) {
+
 }
