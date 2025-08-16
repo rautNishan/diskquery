b0f8040e4bea12551399214a4aa512902cc0aab3
rautNishan <nishan.raut1709@gmail.com>
2025-08-16 12:26:31 +0545

    Port init

--- connection/handleConnection.go
+++ connection/handleConnection.go
@@ -2,8 +2,50 @@ package connection
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"time"
 )
+
+const SEND_BUFFER_SIZE = 8192
+const RECEVE_BUFFER_SIZE = 8192
+
+type Port struct {
+	Conn       net.Conn
+	RemoteAddr net.Addr
+	LocalAddr  net.Addr
+	SendBuffer []byte
+	RecvBuffer []byte
+
+	//Buffer state tracking
+	SendPointer int //Current position in send buffer
+	SendStart   int //Start of unsent data
+	RecvPointer int //Current position in receive buffer
+	RecvLength  int //Amount of data in receive buffer
+
+	//Communication state
+	CommBusy       bool //Busy sending data
+	ReadingMessage bool //In middle of reading a message
+
+	//Connection Properties
+	IsNonBlocking bool
+	TCPNoDelay    bool
+	TCPKeepAlive  bool
+
+	//Protocol version /state
+	ProtocolVersion int
+	SSLActive       bool
+	GSSActive       bool
+
+	//Session State
+	DatabaseName string
+	UserName     string
+
+	ReadDeadLine  time.Time
+	WriteDeadLine time.Time
+
+	BackendPID int
+}
 
 /*
 *
@@ -13,11 +55,59 @@ import (
 *
 */
 func HandelConnection(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Printf("Error while reading from connection: %v", err)
+	// Initialize the port
+	port := init_port(conn)
+	if port.RemoteAddr == nil {
+		log.Fatal("Failed to get remote address")
 	}
-	arrayBuffer := buffer[:n]
-	fmt.Println("This is incoming data: ", string(arrayBuffer))
+	fmt.Print("Port is initialize")
+}
+
+func init_port(conn net.Conn) *Port {
+	port := &Port{
+		Conn:           conn,
+		RemoteAddr:     conn.RemoteAddr(),
+		LocalAddr:      conn.LocalAddr(),
+		SendBuffer:     make([]byte, SEND_BUFFER_SIZE),
+		RecvBuffer:     make([]byte, RECEVE_BUFFER_SIZE),
+		SSLActive:      false,
+		GSSActive:      false,
+		BackendPID:     0,
+		SendPointer:    0,
+		SendStart:      0,
+		RecvPointer:    0,
+		RecvLength:     0,
+		CommBusy:       false,
+		ReadingMessage: false,
+		IsNonBlocking:  false,
+	}
+	err := configureTCPSocket(port)
+	if err != nil {
+		log.Printf("Warning: failed to configure TCP socket: %v", err)
+	}
+	return port
+}
+
+func configureTCPSocket(port *Port) error {
+	tcpConn, ok := port.Conn.(*net.TCPConn)
+	if !ok {
+		//Not a tcp connection (Maybe Unix socket), skip
+		return nil
+	}
+
+	//Enable TCP_NODELAY (Disable Nagle Algorith)
+	if err := tcpConn.SetNoDelay(true); err != nil {
+		return fmt.Errorf("failed to set TCP keepalive: %v", err)
+	}
+
+	port.TCPNoDelay = true
+
+	//Enable TCP Keep Alive
+	if err := tcpConn.SetKeepAlivePeriod(2 * time.Hour); err != nil {
+		return fmt.Errorf("failed to set keepalive period: %v", err)
+	}
+	port.TCPKeepAlive = true
+
+	log.Printf("TCP socket configured: NoDelay=%v KeepAlive=%v", port.TCPNoDelay, port.TCPKeepAlive)
+	return nil
 }
